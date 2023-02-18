c0397a9155ccf5e676d70c9c2949aab687580de1
Ahmed hekal <ahekal369@gmail.com>
2023-02-18 23:14:21 +0100

    dev : build

--- src/lib/Consumer.js
+++ src/lib/Consumer.js
@@ -1,34 +1,31 @@
 "use strict";
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _EventConsumer_instances, _EventConsumer_processEvent;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.EventConsumer = void 0;
 const eventsource_1 = __importDefault(require("eventsource"));
 class EventConsumer {
+    opts;
+    baseUrl;
+    topics;
+    eventSource;
+    callBackEvents = {
+        error: (err) => {
+            console.error(err);
+        },
+        message: (msg) => {
+            console.log(msg);
+        },
+        open: (e) => {
+            console.log(e);
+        }
+    };
     constructor(baseUrl, opts) {
-        _EventConsumer_instances.add(this);
-        this.callBackEvents = {
-            error: (err) => {
-                console.error(err);
-            },
-            message: (msg) => {
-                console.log(msg);
-            },
-            open: (e) => {
-                console.log(e);
-            }
-        };
         this.baseUrl = baseUrl;
         this.opts = {
-            retryConnectionInterval: typeof (opts === null || opts === void 0 ? void 0 : opts.retryConnectionInterval) != "undefined" ? opts === null || opts === void 0 ? void 0 : opts.retryConnectionInterval : 0,
-            headers: typeof (opts === null || opts === void 0 ? void 0 : opts.retryConnectionInterval) != "undefined" ? opts === null || opts === void 0 ? void 0 : opts.headers : undefined,
+            retryConnectionInterval: typeof opts?.retryConnectionInterval != "undefined" ? opts?.retryConnectionInterval : 0,
+            headers: typeof opts?.retryConnectionInterval != "undefined" ? opts?.headers : undefined,
         };
     }
     startConsumeFromTopics(topics) {
@@ -39,33 +36,32 @@ class EventConsumer {
         else {
             _topics = `topic=${topics}`;
         }
-        this.eventSource = new eventsource_1.default(`${this.baseUrl}/consume?${_topics}`, Object.assign({}, this.opts.headers));
+        this.eventSource = new eventsource_1.default(`${this.baseUrl}/consume?${_topics}`, { ...this.opts.headers });
         this.eventSource.onopen = (evt) => {
-            __classPrivateFieldGet(this, _EventConsumer_instances, "m", _EventConsumer_processEvent).call(this, "open", evt);
+            this.#processEvent("open", evt);
         };
         this.eventSource.onmessage = (evt) => {
-            __classPrivateFieldGet(this, _EventConsumer_instances, "m", _EventConsumer_processEvent).call(this, "message", evt);
+            this.#processEvent("message", evt);
         };
         this.eventSource.onopen = (evt) => {
-            __classPrivateFieldGet(this, _EventConsumer_instances, "m", _EventConsumer_processEvent).call(this, "open", evt);
+            this.#processEvent("open", evt);
         };
+    }
+    #processEvent(type, event) {
+        const obj = Object.assign({}, { ...event });
+        if (type == "error") {
+            typeof this.callBackEvents?.error == "function" && this.callBackEvents?.error(obj);
+        }
+        else if (type == "message") {
+            obj.data = JSON.parse(obj.data).event;
+            typeof this.callBackEvents?.message == "function" && this.callBackEvents?.message(obj);
+        }
+        else if (type == "open") {
+            typeof this.callBackEvents?.open == "function" && this.callBackEvents?.open(obj);
+        }
     }
     on(type, cb) {
         this.callBackEvents[type] = cb;
     }
 }
 exports.EventConsumer = EventConsumer;
-_EventConsumer_instances = new WeakSet(), _EventConsumer_processEvent = function _EventConsumer_processEvent(type, event) {
-    var _a, _b, _c, _d, _e, _f;
-    const obj = Object.assign({}, Object.assign({}, event));
-    if (type == "error") {
-        typeof ((_a = this.callBackEvents) === null || _a === void 0 ? void 0 : _a.error) == "function" && ((_b = this.callBackEvents) === null || _b === void 0 ? void 0 : _b.error(obj));
-    }
-    else if (type == "message") {
-        obj.data = JSON.parse(obj.data).event;
-        typeof ((_c = this.callBackEvents) === null || _c === void 0 ? void 0 : _c.message) == "function" && ((_d = this.callBackEvents) === null || _d === void 0 ? void 0 : _d.message(obj));
-    }
-    else if (type == "open") {
-        typeof ((_e = this.callBackEvents) === null || _e === void 0 ? void 0 : _e.open) == "function" && ((_f = this.callBackEvents) === null || _f === void 0 ? void 0 : _f.open(obj));
-    }
-};
--- src/lib/Publisher.js
+++ src/lib/Publisher.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,28 +6,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.EventPublisher = void 0;
 const node_fetch_1 = __importDefault(require("node-fetch"));
 class EventPublisher {
+    baseUrl;
     constructor(baseUrl) {
         this.baseUrl = baseUrl;
     }
-    publish(topic, message) {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                yield (0, node_fetch_1.default)(`${this.baseUrl}/sendMessageToTopic`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        'topic': topic,
-                        'message_body': message
-                    })
-                });
-                //const data = await  res.json();
-            }
-            catch (err) {
-                throw err;
-            }
-        });
+    async publish(topic, message) {
+        try {
+            await (0, node_fetch_1.default)(`${this.baseUrl}/sendMessageToTopic`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'topic': topic,
+                    'message_body': message
+                })
+            });
+            //const data = await  res.json();
+        }
+        catch (err) {
+            throw err;
+        }
     }
 }
 exports.EventPublisher = EventPublisher;
