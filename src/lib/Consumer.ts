1b32b84a61f4d38855898424b385a0036c5da94a
Ahmed Hekal <ahekal@movia.biz>
2024-12-26 17:01:15 +0100

    improve: support type inaction for message or topic

--- src/lib/Consumer.ts
+++ src/lib/Consumer.ts
@@ -1,94 +1,85 @@
 import EventSource from 'eventsource'
 import { CallBackEvents } from '../interfaces'
 
-
-
 export class EventConsumer {
-    opts: {
-        retryConnectionInterval: number,
-        headers?: { [key: string]: string }
+  opts: {
+    retryConnectionInterval: number
+    headers?: { [key: string]: string }
+  }
+
+  baseUrl: string
+  topics?: string | string[]
+  eventSource?: EventSource
+  callBackEvents: {
+    error: (err: { type: 'error'; message: string }) => void
+    message: (msg: { type: 'message'; message: string }) => void
+    open: (e: { type: 'open' }) => void
+  } = {
+    error: err => {
+      console.error(err)
+    },
+    message: msg => {
+      console.log(msg)
+    },
+    open: e => {
+      console.log(e)
+    }
+  }
+
+  constructor(baseUrl: string, opts?: { retryConnectionInterval?: number; headers?: { [key: string]: string } }) {
+    this.baseUrl = baseUrl
+    this.opts = {
+      retryConnectionInterval: typeof opts?.retryConnectionInterval != 'undefined' ? opts?.retryConnectionInterval : 0,
+      headers: typeof opts?.retryConnectionInterval != 'undefined' ? opts?.headers : undefined
+    }
+  }
+
+  startConsumeFromTopics<TopicsType = string>(topics: TopicsType | TopicsType[]) {
+    let _topics = ''
+    if (Array.isArray(topics)) {
+      _topics = topics.map(p => `topic=${p}`).join('&')
+    } else {
+      _topics = `topic=${topics}`
     }
 
-    baseUrl: string
-    topics?: string | string[]
-    eventSource?: EventSource
-    callBackEvents: {
-        error: (err: { type: "error", message: string }) => void,
-        message: (msg: { type: "message", message: string }) => void,
-        open: (e: { type: "open" }) => void
-    } = {
-            error: (err) => {
-                console.error(err)
-            },
-            message: (msg) => {
-                console.log(msg)
-            },
-            open: (e) => {
-                console.log(e)
-            }
-        }
-
-
-    constructor(baseUrl: string, opts?: { retryConnectionInterval?: number, headers?: { [key: string]: string } }) {
-        this.baseUrl = baseUrl;
-        this.opts = {
-            retryConnectionInterval: typeof opts?.retryConnectionInterval != "undefined" ? opts?.retryConnectionInterval : 0,
-            headers: typeof opts?.retryConnectionInterval != "undefined" ? opts?.headers : undefined,
-        }
+    this.eventSource = new EventSource(`${this.baseUrl}/consume?${_topics}`, { ...this.opts.headers })
+    this.eventSource.onopen = (evt: any) => {
+      this.#processEvent('open', evt)
     }
-
-
-
-    startConsumeFromTopics(topics: string | string[]) {
-        let _topics = ''
-        if (Array.isArray(topics)) {
-            _topics = topics.map(p => `topic=${p}`).join("&")
-        } else {
-            _topics = `topic=${topics}`
-        }
-
-        this.eventSource = new EventSource(`${this.baseUrl}/consume?${_topics}`, { ...this.opts.headers });
-        this.eventSource.onopen = (evt: any) => {
-            this.#processEvent("open", evt)
-        }
-        this.eventSource.onmessage = (evt: any) => {
-            this.#processEvent("message", evt)
-        }
-        this.eventSource.onopen = (evt: any) => {
-            this.#processEvent("open", evt)
-        }
-
+    this.eventSource.onmessage = (evt: any) => {
+      this.#processEvent('message', evt)
     }
-
-
-    #processEvent(type: CallBackEvents, event: any) {
-        const obj = Object.assign({}, { ...event })
-        if (type == "error") {
-            typeof this.callBackEvents?.error == "function" && this.callBackEvents?.error(obj)
-        } else if (type == "message") {
-            obj.data = JSON.parse(obj.data).event
-            typeof this.callBackEvents?.message == "function" && this.callBackEvents?.message(obj)
-        } else if (type == "open") {
-            typeof this.callBackEvents?.open == "function" && this.callBackEvents?.open(obj)
-        }
+    this.eventSource.onopen = (evt: any) => {
+      this.#processEvent('open', evt)
     }
+  }
 
-
-
-    public on(type: "error", cb: (err: { type: "error", message: string }) => void): void;
-    public on(type: "message", cb: (msg: {
-        type: "message",
-        data: {
-            timestamp: Date,
-            event: any,
-            topic: string
-        }
-    }) => void): void;
-    public on(type: "open", cb: (e: { type: "open" }) => void): void;
-    public on(type: CallBackEvents, cb: any) {
-        this.callBackEvents[type] = cb
+  #processEvent(type: CallBackEvents, event: any) {
+    const obj = Object.assign({}, { ...event })
+    if (type == 'error') {
+      typeof this.callBackEvents?.error == 'function' && this.callBackEvents?.error(obj)
+    } else if (type == 'message') {
+      obj.data = JSON.parse(obj.data).event
+      typeof this.callBackEvents?.message == 'function' && this.callBackEvents?.message(obj)
+    } else if (type == 'open') {
+      typeof this.callBackEvents?.open == 'function' && this.callBackEvents?.open(obj)
     }
+  }
 
-
-
+  public on(type: 'error', cb: (err: { type: 'error'; message: string }) => void): void
+  public on(
+    type: 'message',
+    cb: (msg: {
+      type: 'message'
+      data: {
+        timestamp: Date
+        event: any
+        topic: string
+      }
+    }) => void
+  ): void
+  public on(type: 'open', cb: (e: { type: 'open' }) => void): void
+  public on(type: CallBackEvents, cb: any) {
+    this.callBackEvents[type] = cb
+  }
 }
--- src/lib/Publisher.ts
+++ src/lib/Publisher.ts
@@ -1,45 +1,38 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 export class EventPublisher {
-    baseUrl: string
-    debugMode:boolean
-    constructor(baseUrl: string,debugMode:boolean = false) {
-        this.baseUrl = baseUrl;
-        this.debugMode = debugMode
-    }
+  baseUrl: string
+  debugMode: boolean
+  constructor(baseUrl: string, debugMode: boolean = false) {
+    this.baseUrl = baseUrl
+    this.debugMode = debugMode
+  }
 
+  async publish<MessageType = any, TopicsType = string>(topic: TopicsType, message: MessageType) {
+    try {
+      const res = await fetch(`${this.baseUrl}/sendMessageToTopic`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          topic: topic,
+          message_body: message
+        })
+      })
 
-    async publish(topic: string, message: any) {
+      if (this.debugMode) {
         try {
-            const res = await fetch(`${this.baseUrl}/sendMessageToTopic`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    'topic': topic,
-                    'message_body': message
-                })
-            });
-
-            if (this.debugMode) {
-                try {
-                    
-                    const data = await  res.json();
-                    if (data) {
-                         console.log(`sent response : ${JSON.stringify(data,null,'\t')}`)
-                    }
-
-
-                } catch (therr) {
-                    console.error(`server return 200 ok i got error while parsing response`)
-                }
-            }
-
-
-        } catch (err) {
-            throw err
+          const data = await res.json()
+          if (data) {
+            console.log(`sent response : ${JSON.stringify(data, null, '\t')}`)
+          }
+        } catch (therr) {
+          console.error(`server return 200 ok i got error while parsing response`)
         }
-
+      }
+    } catch (err) {
+      throw err
     }
+  }
 }
